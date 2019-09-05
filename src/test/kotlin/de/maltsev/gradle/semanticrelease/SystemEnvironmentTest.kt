65cf49dbe90f92a822ff7e494cc9ddb3b5f89e68
Evgeniy Maltsev <low205@gmail.com>
2019-09-05 10:02:06 +0200

    test: fix SystemEnvironmentTest

--- src/test/kotlin/de/maltsev/gradle/semanticrelease/SystemEnvironmentTest.kt
+++ src/test/kotlin/de/maltsev/gradle/semanticrelease/SystemEnvironmentTest.kt
@@ -1,23 +1,16 @@
 package de.maltsev.gradle.semanticrelease
 
 import io.kotlintest.extensions.system.withEnvironment
-import io.kotlintest.specs.ShouldSpec
 import io.kotlintest.shouldBe
+import io.kotlintest.specs.ShouldSpec
 
 class SystemEnvironmentTest : ShouldSpec() {
 
     init {
         should("read from System.env") {
-            withEnvironment(mapOf(
-                "FooKey" to "BarValue",
-                "GITHUB_TOKEN" to "token",
-                "TRAVIS" to "true"
-            )) {
+            withEnvironment(mapOf("TestFooKey" to "TestBarValue")) {
                 val env = SystemEnvironment()
-                env.getEnv("FooKey") shouldBe "BarValue"
-                env.gitHubToken shouldBe "token"
-                env.hasGitHub() shouldBe true
-                env.hasTravis() shouldBe true
+                env.getEnv("TestFooKey") shouldBe "TestBarValue"
             }
         }
     }
